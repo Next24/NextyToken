452083ae2c4f4257b05f964316147e53a75015ae
Next24 <140563186+Next24@users.noreply.github.com>
2023-07-31 16:01:04 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -185,8 +185,8 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to NEXTY Token BURNER DAPP!</h1>
-          <p><h3>whith my Web3 dApp you can use the Base Main Network</h3></p>
+          <h1 className={styles.title}>Welcome to <a href="x.com/Next_24h">NEXTY</a> Token BURNER DAPP!</h1>
+          <p><h2 className={styles.description}>whith my Web3 dApp you can use the Base Main Network</h2></p>
           
           {walletConnected ? (
             <div>
