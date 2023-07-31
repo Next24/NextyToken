fbf0c30f69259fe8bbf0b10a18eb61a2bf874480
Next24 <140563186+Next24@users.noreply.github.com>
2023-07-31 15:34:05 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xfc99cfe7e105fb0a48df1bec15101e82e09d5c2c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
